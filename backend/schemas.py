1073f120d12e6e16e3e2cae928cb438ef94bdcb9
Harshil-Jani <harshiljani2002@gmail.com>
2024-05-03 03:47:54 +0530

    Add Optional to relevant schema
    Signed-off-by: Harshil-Jani <harshiljani2002@gmail.com>

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,22 +1,24 @@
 # Datatype validation schema using pydantic
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 from uuid import UUID
 from datetime import datetime, date
 
 class NurseBase(BaseModel):
-    nurse_id: UUID
+    nurse_id: Optional[UUID]
     nurse_name: str
     nurse_email: str
     nurse_city: str
-    nurse_created_at: datetime
+    nurse_created_at: Optional[datetime]
     nurse_dob: date
     nurse_phone_number: str
 
 class PatientBase(BaseModel):
-    patient_id: UUID
+    patient_id: Optional[UUID]
     patient_name: str
     patient_email: EmailStr
-    patient_created_at: datetime
+    patient_created_at: Optional[datetime]
     patient_phone_number: str
     patient_address: str
+    patient_dob: date
     patient_city : str
