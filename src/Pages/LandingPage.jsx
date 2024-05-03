1db0c2f299a7994b0063cbc01c4f5b74cab2b87e
SSK <saikumar.sodipilli@gmail.com>
2024-05-04 05:21:38 +0530

    fix: modified the learn more button to scroll to the section

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -16,11 +16,13 @@ function LandingPage() {
               Empower Indian Nurses
             </h4>
             <h2 className="max-w-[16ch] m-auto sm:text-6xl lxg:ml-0 font-bold text-5xl mb-5">
-              <span className="text-green-500">Book your Home Care</span> now using Nacto Care
+              <span className="text-green-500">Book your Home Care</span> now
+              using Nacto Care
             </h2>
             <p className="max-w-[55ch] m-auto lxg:ml-0 mb-6">
-              Find the nurse you need for your loved ones at home. Professional Cure,
-              personalized care plans, flexible scheduling, health insurance and much more.
+              Find the nurse you need for your loved ones at home. Professional
+              Cure, personalized care plans, flexible scheduling, health
+              insurance and much more.
             </p>
             <div className="mt-12 sm:mt-9 flex items-center flex-col sm:flex-row sm:justify-center  lxg:justify-start sm:gap-4">
               <a href="#block3">
@@ -28,12 +30,12 @@ function LandingPage() {
                   Book Nurse
                 </button>
               </a>
-              <Link to="/about"> 
-              {/* TODO : Change this about to some block below since we are not planning an about page */}
+              <a href="#block2">
+                {/* TODO : Change this about to some block below since we are not planning an about page */}
                 <button className="bg-black w-48 mb-4 sm:mb-0 sm:min-w-[10rem] transition-all shadow-lg shadow-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:scale-105 text-white px-12 py-3 rounded-sm">
                   Learn More
                 </button>
-              </Link>
+              </a>
             </div>
           </div>
           <div className="py-10  items-end hidden lxg:flex px-7">
@@ -54,7 +56,9 @@ function LandingPage() {
         <div className="px-8 lg:px-16  sm:mt-28 md:mt-28 mb-28" id="block2">
           <div className="text-center mb-14 ">
             <h3 className="text-3xl font-medium">Book a health checkup now</h3>
-            <h2 className="text-5xl font-bold">Quick & easy care for elderly and your loved ones</h2>
+            <h2 className="text-5xl font-bold">
+              Quick & easy care for elderly and your loved ones
+            </h2>
           </div>
           <div className="flex flex-col  m-auto max-w-[75rem] lg:flex-row justify-evenly mt-14 lg:mt-28">
             <div className="flex flex-col items-center">
@@ -68,8 +72,9 @@ function LandingPage() {
               <div className="text-center mt-8">
                 <h2 className="text-2xl font-bold">Professional Nurse</h2>
                 <p className="max-w-[36ch] mt-3 text-gray-500 font-medium">
-                We provide a wide selection of nurses for all your healthcare needs.
-We have the perfect caregiver to meet your requirements.
+                  We provide a wide selection of nurses for all your healthcare
+                  needs. We have the perfect caregiver to meet your
+                  requirements.
                 </p>
               </div>
             </div>
@@ -102,7 +107,8 @@ We have the perfect caregiver to meet your requirements.
               <div className="text-center mt-8">
                 <h2 className="text-2xl font-bold">We Care for you</h2>
                 <p className="max-w-[40ch] mt-3 text-gray-500 font-medium">
-                Personalized care tailored to you, with comprehensive health insurance coverage.
+                  Personalized care tailored to you, with comprehensive health
+                  insurance coverage.
                 </p>
               </div>
             </div>
@@ -128,13 +134,20 @@ We have the perfect caregiver to meet your requirements.
                   We value for health, healthcare workers and the nation
                 </h2>
                 <p className="w-full text-center smd:text-left max-w-[65ch] my-2 text-zinc-600">
-                We believe in valuing every aspect of health, from individual wellness to the tireless dedication of those 
-                who nurture it. Nurses in India are migrating to different nation to find better opportunities but we are on 
-                a mission to provide them work opportunities by fulfilling the needs of home nursing as a freelancing solution
-                and connect the patients with certified nurses.
+                  We believe in valuing every aspect of health, from individual
+                  wellness to the tireless dedication of those who nurture it.
+                  Nurses in India are migrating to different nation to find
+                  better opportunities but we are on a mission to provide them
+                  work opportunities by fulfilling the needs of home nursing as
+                  a freelancing solution and connect the patients with certified
+                  nurses.
                 </p>
                 <p className="w-full text-center smd:text-left max-w-[65ch] my-2 text-zinc-600">
-                For the care seekers, We provide them personalized healthcare care plans and health insurances which will protect them in case of emergency. Covering both the aspects of care seekers and care givers we are aiming to solve the health care for world's largest democratic nation.
+                  For the care seekers, We provide them personalized healthcare
+                  care plans and health insurances which will protect them in
+                  case of emergency. Covering both the aspects of care seekers
+                  and care givers we are aiming to solve the health care for
+                  world's largest democratic nation.
                 </p>
               </div>
             </div>
@@ -153,18 +166,15 @@ We have the perfect caregiver to meet your requirements.
                     Certified Professional Healthcare workers
                   </h2>
                   <p className="max-w-[38ch] text-zinc-500 font-medium text-xm">
-                  We ensure quality care by connecting you with certified healthcare professionals you can trust. Your well-being is our priority.
+                    We ensure quality care by connecting you with certified
+                    healthcare professionals you can trust. Your well-being is
+                    our priority.
                   </p>
                 </div>
               </div>
               <div className="flex flex-col  gap-3 items-center  smd:gap-5 smd:flex smd:flex-row max-w-[70ch] mb-4">
                 <div>
-                  <img
-                    src="\assets\debt.png"
-                    height={100}
-                    width={100}
-                    alt=""
-                  />
+                  <img src="\assets\debt.png" height={100} width={100} alt="" />
                 </div>
                 <div className="max-w-[70ch] mb-3">
                   <h2 className="text-zinc-700 font-bold text-xl">
@@ -172,7 +182,8 @@ We have the perfect caregiver to meet your requirements.
                   </h2>
                   <p className="max-w-[38ch] text-zinc-500 font-medium text-xm">
                     Enjoy peace of mind with our no hidden charges policy. We
-                    believe in transparent and honest pricing and what you see will be the last amount that you'd ever pay.
+                    believe in transparent and honest pricing and what you see
+                    will be the last amount that you'd ever pay.
                   </p>
                 </div>
               </div>
@@ -185,7 +196,8 @@ We have the perfect caregiver to meet your requirements.
                     Get Care, Anywhere
                   </h2>
                   <p className="max-w-[38ch] text-zinc-500 font-medium text-xm">
-                    We are constantly expanding our service across cities in India
+                    We are constantly expanding our service across cities in
+                    India
                   </p>
                 </div>
               </div>
