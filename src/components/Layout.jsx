cc4098b020f534e16f496dd4b8b924ad1b65ccb6
Antriksh <antriksh.scin9@gmail.com>
2024-05-23 14:54:22 +0530

    added-chatbot

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import { Helmet } from "react-helmet";
 
 function Layout() {
   return (
@@ -9,6 +10,10 @@ function Layout() {
       <Header />
       <Outlet />
       <Footer />
+      <Helmet>
+        <script src="https://cdn.botpress.cloud/webchat/v2/inject.js" defer async></script>
+        <script src="https://mediafiles.botpress.cloud/7b050b54-3832-4502-b938-a6a018f8f015/webchat/v2/config.js" defer async></script>
+      </Helmet>
     </div>
   );
 }
