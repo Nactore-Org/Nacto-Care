a6f5d905aa0a833ee6e88b4d93f15cca4c0b4efd
Ramakrushna Biswal <125277258+RamakrushnaBiswal@users.noreply.github.com>
2024-06-06 21:37:30 +0530

    Update NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
     return (
         <div>
-            <img src="./assets/404.png" alt="404" className="w-1/3 mx-auto " />
+            <img src="./assets/404.webp" alt="404" className="w-1/3 mx-auto " />
             <Link to="/" className="text-white bg-green-500 mx-auto text-2xl justify-center flex items-center px-4 py-3 rounded-2xl w-1/6">Go Home</Link>
         </div>
     );
